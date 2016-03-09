fdc22b56eb4ff02fa80a606d93992f4fb839f202
Carl Eichhorn <ceichhorn@gmti.gannett.com>
2016-03-09 11:31:16 -0500

    added email script

---
+++ email-build.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+(
+echo "EHLO HOSTNAME"
+echo "mail from:<FROM>"
+echo "rcpt to:<TO>"
+
+sleep 1
+
+echo "data"
+
+sleep 1
+echo "From: EMAIL"
+echo "To: EMAIL"
+echo "subject: ${BRANCH_NAME} Feature build successful"
+echo ""
+sleep 1
+echo
+echo "${FULL_REPO_NAME}"
+echo " Build was created in ${ARTIFACTORY_URL}/${REPOSITORY_NAME}-${BUILD_NUMBER}.tar.gz"
+sleep 1
+echo ""
+echo "Your branch name: ${BRANCH_NAME}"
+echo "From repo:        ${REPOSITORY_NAME}"
+echo "%teamcity.build.triggeredBy%"
+
+echo "."
+
+sleep 2
+
+echo "exit"
+
+) | telnet smtp 25
+exit 0
